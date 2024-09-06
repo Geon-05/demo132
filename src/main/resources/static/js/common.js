b5f6e66a4a7fe7be526aebb1ba8767d6e90f592e
geon <zqrc05@gmail.com>
2024-09-06 18:22:21 +0900

    페이지네이션 드디어 해결

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,4 +1,7 @@
-function go(pageNo){
-  searchForm.pageNo.value = pageNo;
+function go(pageNo_input){
+  let pageNo = document.querySelector('#pageNo');
+  let searchForm = document.querySelector('#searchForm')
+  
+  pageNo.value = pageNo_input;
   searchForm.submit();
 }
