c07ab992886dec0df227e534b175311b4feef82d
geon <zqrc05@gmail.com>
2024-09-07 18:56:21 +0900

    도서생성에서 에러메세지 동적으로 구현중

--- src/main/resources/static/js/book/insert.js
+++ src/main/resources/static/js/book/insert.js
@@ -1,10 +1,10 @@
-let validation_chk = false;
+let validation_chk = true;
 
 insert_book();
 
 function insert_book() {
   let btn_addBook = document.querySelector("#btn_addBook");
-  
+
   btn_addBook.addEventListener("click", function () {
     if (validation()) {
       insertForm.submit();
@@ -17,25 +17,41 @@ function validation() {
   let input_author = document.querySelector("#input_author");
   let input_price = document.querySelector("#input_price");
   let input_public = document.querySelector("#input_public");
-  let input_file = document.querySelector('#input_file');
+  let input_file = document.querySelector("#input_file");
+
+  let msgBox = "";
+  let msgId = "";
   let extensions = ["jpg", "gif", "png"];
 
   if (input_title.value == "") {
-    alert("도서 타이틀을 입력하세요.");
-    return validation_chk=false;
-  } else if (input_author.value == "") {
-    alert("작가명을 입력하세요.");
-    return validation_chk=false;
-  } else if (input_price.value == "") {
-    alert("도서 금액 입력하세요.");
-    return validation_chk=false;
-  } else if (input_public.value == "") {
+    msgBoxInsert("도서 타이틀을 입력하세요.", "input_title");
+    validation_chk = false;
+  }
+
+  if(input_author.value == "") {
+    msgBoxInsert("작가명을 입력하세요.", "input_author");
+    validation_chk = false;
+  }
+  
+  if (input_price.value == "") {
+    msgBoxInsert("도서 금액 입력하세요.", "input_price");
+    validation_chk = false;
+  }
+  
+  if (input_public.value == "") {
     let res = confirm("출판사를 미기입합니까?");
     if (res) {
-      return validation_chk=true;
+      validation_chk = true;
     } else {
-      return validation_chk=false;
+      validation_chk = false;
     }
   }
-  return validation_chk=true;
+}
+
+function msgBoxInsert(msgBox, msgId) {
+  let existingMsg = document.querySelector(`#${msgId} + p`);
+  if (existingMsg) {
+    existingMsg.remove();
+  }
+  document.getElementById(msgId).insertAdjacentHTML('afterend', `<p id='msgBox'>${msgBox}</p>`);
 }
